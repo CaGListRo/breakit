08c4c03c76e6fee3d004f18362bda6e29d5c8264
CaGListRo <paco.nyns@gmail.com>
2024-12-12 11:33:00 +0100

    WIP: Added type hints, return type hints and doc strings to the breakit.py and brick.py.

--- breakit.py
+++ breakit.py
@@ -8,61 +8,68 @@ from utilities import load_image, load_images
 import settings as sets
 
 from random import randint
-
+from typing import Final
 
 
 class Game:
-    '''This is the game class, here is the main-loop located in the play-method'''  
-    def __init__(self):
+    """This is the game class, here is the main-loop located in the play-method"""
+    FPS: Final[int] = 60
+    SCORE_FONT: Final[pg.font.Font] = pg.font.SysFont("comicsans", 42)
+    GAME_OVER_FONT: Final[pg.font.Font] = pg.font.SysFont("comicsans", 142)
+
+    def __init__(self) -> None:
+        """ Initializes the game class. """
         pg.init()
-        self.main_window = pg.display.set_mode((sets.MAIN_WINDOW_WIDTH, sets.MAIN_WINDOW_HEIGHT))
-        self.game_window = pg.Surface((sets.GAME_WINDOW_WIDTH, sets.GAME_WINDOW_HEIGHT))
-        self.running = True
-        self.clock = pg.time.Clock()
-        self.FPS = 60
-        self.SCORE_FONT = pg.font.SysFont("comicsans", 42)
-        self.GAME_OVER_FONT = pg.font.SysFont("comicsans", 142)
-        
-        self.game_assets = {
-            'background':load_images(path='backgrounds'),
-            'main_background': load_image(path='main background', img_name='space.png'),
-            'ball': load_image(path='ball', img_name='ball.png'),
-            'bricks': load_images(path='bricks'),
-            'paddle': load_images(path='paddle'),
-            'pwr_ups': load_images(path='power ups')
+        self.main_window: pg.display = pg.display.set_mode((sets.MAIN_WINDOW_WIDTH, sets.MAIN_WINDOW_HEIGHT))
+        self.game_window: pg.Surface = pg.Surface((sets.GAME_WINDOW_WIDTH, sets.GAME_WINDOW_HEIGHT))
+        self.running: bool = True
+        self.clock: pg.time.Clock = pg.time.Clock()
+          
+        self.game_assets: dict[pg.Surface] = {
+            "background":load_images(path="backgrounds"),
+            "main_background": load_image(path="main background", img_name="space.png"),
+            "ball": load_image(path="ball", img_name="ball.png"),
+            "bricks": load_images(path="bricks"),
+            "paddle": load_images(path="paddle"),
+            "pwr_ups": load_images(path="power ups")
         }
 
-        self.score = 0
-        self.level = 1
-        self.extra_lives = 2
-        self.movement = [False, False]
-        self.paddle = Paddle(self, self.game_assets['paddle'][1])
+        self.score: int = 0
+        self.level: int = 1
+        self.extra_lives: int = 2
+        self.movement: list[bool] = [False, False]
+        self.paddle: Paddle = Paddle(self, self.game_assets["paddle"][1])
         self.create_brick_pattern()
-        self.ball = Ball(self, self.game_assets['ball'], self.paddle, self.brick_pattern)
-        self.background_number = randint(0, 7)
-        self.power_ups = []
-        self.power_up_rects = []
+        self.ball: Ball = Ball(self, self.game_assets["ball"], self.paddle, self.brick_pattern)
+        self.background_number: int = randint(0, 7)
+        self.power_ups: list[pg.Surface] = []
+        self.power_up_rects: list[pg.Rect] = []
     
-    def level_complete(self):
+    def level_complete(self) -> None:
+        """ Brings the player to the next level. """
         self.ball.active = False
         self.level += 1
         self.create_brick_pattern()
 
-    def game_over(self):
+    def game_over(self) -> None:
+        """ Closes the game. """
         self.ball.active = False
         self.running = False
 
-    def create_brick_pattern(self):
+    def create_brick_pattern(self) -> None:
+        """ Creates the brick pattern. """
         self.brick_pattern = Brick(self, self.level)
 
-    def handle_power_ups(self):
+    def handle_power_ups(self) -> None:
+        """ Updates the power ups and deletes them when they are of screen. """
         for i, pwr_up in enumerate(self.power_ups):
             pwr_up.update()
             if pwr_up.rect.top > sets.GAME_WINDOW_HEIGHT:
                 self.power_ups.pop(i)
                 self.power_up_rects.pop(i)
 
-    def event_handler(self):
+    def event_handler(self) -> None:
+        """ Handles all the events in the game. """
         for event in pg.event.get():
                 if event.type == pg.QUIT:
                     quit()
@@ -79,17 +86,18 @@ class Game:
                     if event.key == pg.K_LCTRL:
                         self.ball.active = True
 
-    def draw_window(self):
-        self.main_window.blit(pg.transform.scale(self.game_assets['main_background'], (sets.MAIN_WINDOW_WIDTH, sets.MAIN_WINDOW_HEIGHT)), (0, 0))
+    def draw_window(self) -> None:
+        """ Draws all the elements on the main window. """
+        self.main_window.blit(pg.transform.scale(self.game_assets["main_background"], (sets.MAIN_WINDOW_WIDTH, sets.MAIN_WINDOW_HEIGHT)), (0, 0))
         pg.draw.rect(self.main_window, (242, 242, 242), (95, 95, 1410, 710))
 
         score_to_blit = self.SCORE_FONT.render(str(self.score), True, "white")
         self.main_window.blit(score_to_blit, (sets.MAIN_WINDOW_WIDTH // 2 - score_to_blit.get_width() // 2, 800))
 
         for i in range(self.extra_lives):
-            self.main_window.blit(self.game_assets['paddle'][1], (100 + i * 300, 45))
+            self.main_window.blit(self.game_assets["paddle"][1], (100 + i * 300, 45))
         
-        self.game_window.blit(self.game_assets['background'][self.background_number], (0, 0))
+        self.game_window.blit(self.game_assets["background"][self.background_number], (0, 0))
 
         for pwr_up in self.power_ups:
             pwr_up.render(self.game_window)
@@ -101,7 +109,8 @@ class Game:
         self.main_window.blit(self.game_window, (100, 100))
         pg.display.update()
 
-    def run(self):
+    def run(self) -> None:
+        """ Runs the game and contains the game loop. """
         while self.running:
             self.clock.tick(self.FPS)
             
@@ -117,5 +126,5 @@ class Game:
             
             self.draw_window()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     Game().run()
--- brick.py
+++ brick.py
@@ -1,32 +1,52 @@
 import pygame as pg
 import breakit_pattern as bip
 
+from typing import Final, TypeVar
+
+Game = TypeVar("Game")
+
 
 class Brick:
-    '''the brick class should handle the brick related stuff, like the position and health of the brick'''
-    def __init__(self, game, level):
-        self.game = game
-        self.level = level
-        self.brick_list = []
+    """the brick class should handle the brick related stuff, like the position and health of the brick"""
+    BRICK_X_DISTANCE: Final[int] = 140
+    BRICK_Y_DISTANCE: Final[int] = 48
+
+    def __init__(self, game: Game, level: int) -> None:
+        """
+        Initializes the Brick object.
+        Args:
+        game (Game): The game object.
+        level (int): The current level.
+        """
+        self.game: Game = game
+        self.level: int = level
+        self.brick_list: list[list[int]] = []
         self.brick_rects = []
         self.load_pattern()
 
-    def create_bricks(self):
+    def create_bricks(self) -> None:
+        """ Creates the brick_list and the brick_rects list. """
         for i in range(10):
             for j in range(5):
                 if self.level_pattern[j][i] > 0:
-                    self.brick_list.append([i * 140, j * 48, self.level_pattern[j][i]])
-                    self.brick_rects.append(pg.Rect(i * 140, j * 48, 140, 48))
-        print(f"bl: {self.brick_list}     br: {self.brick_rects}")
+                    self.brick_list.append([i * self.BRICK_X_DISTANCE, j * self.BRICK_Y_DISTANCE, self.level_pattern[j][i]])
+                    self.brick_rects.append(pg.Rect(i * self.BRICK_X_DISTANCE, j * self.BRICK_Y_DISTANCE, self.BRICK_X_DISTANCE, self.BRICK_Y_DISTANCE))
+        # print(f"bl: {self.brick_list}     br: {self.brick_rects}")
 
-    def load_pattern(self):
-        pattern_name = 'level' + str(self.level) + '_pattern'
+    def load_pattern(self) -> None:
+        """ Loads the brick pattern from the breakit_pattern.py (bip)"""
+        pattern_name = "level" + str(self.level) + "_pattern"
         self.level_pattern = getattr(bip, pattern_name)
         self.create_bricks()
 
-    def render(self, surf):
+    def render(self, surf: pg.Surface) -> None:
+        """
+        Draws the bricks to the given surface.
+        Args:
+        serf (pg.Surface): The surface to draw the bricks on.
+        """
         for brick in self.brick_list:
             if brick[2] > 0:
-                surf.blit(self.game.game_assets['bricks'][brick[2] - 1], (brick[0], brick[1]))
+                surf.blit(self.game.game_assets["bricks"][brick[2] - 1], (brick[0], brick[1]))
         for rectangle in self.brick_rects:
             pg.draw.rect(surf, (255, 0,  0), rectangle, 3)
