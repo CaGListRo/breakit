d2957eb5ef2d987806a83c358fd848ec40e8006b
CaGListRo <paco.nyns@gmail.com>
2024-12-14 11:50:28 +0100

    WIP: Added type hints, return type hints and doc strings to the paddle.py.

--- paddle.py
+++ paddle.py
@@ -1,15 +1,28 @@
 import pygame as pg
 import settings as sets
 
+from typing import Final, TypeVar
+
+Game = TypeVar("Game")
+
 class Paddle:
-    '''the paddle class handles all the movements and collisions of the paddle'''
-    def __init__(self, game, image):
-        self.game = game
-        self.image = image
-        self.rect = self.image.get_rect(midtop=(700, 650))
-        self.pos = pg.math.Vector2(self.rect.topleft)
+    """ The paddle class handles the movements of the paddle and the collisions with power ups. """
+    MID_TOP: Final[tuple[int]] = (700, 650)
+
+    def __init__(self, game: Game, image: pg.Surface) -> None:
+        """
+        Initializes the paddle object.
+        Args:
+        game (Game): The game object.
+        image (pg.Surface): The image of the paddle.
+        """
+        self.game: Game = game
+        self.image: pg.Surface = image
+        self.rect: pg.Rect = self.image.get_rect(midtop=self.MID_TOP)
+        self.pos: pg.Vector2 = pg.Vector2(self.rect.topleft)
         
-    def check_pwr_up_collision(self):
+    def check_pwr_up_collision(self) -> None:
+        """ Checks if the paddle collides with a power up. """
         for i, pwr_up in enumerate(self.game.power_up_rects.copy()):
             if self.rect.colliderect(pwr_up[0]):
                 if pwr_up[1] == 0 and self.game.ball.speed > 1:
@@ -34,7 +47,13 @@ class Paddle:
                 self.game.power_ups.pop(i)
                 self.game.power_up_rects.pop(i)
 
-    def update(self, movement=(0, 0), paddle_size=200):
+    def update(self, movement: tuple[int] = (0, 0), paddle_size: int = 200) -> None:
+        """
+        Updates the paddle position based on the movement and the paddle size.
+        Args:
+        movement (tuple[int]): The movement of the paddle. Defaults to (0, 0).
+        paddle_size (int): The size of the paddle. Defaults to 200.
+        """
         self.pos.x += (movement[1] - movement[0]) * 10
         self.rect.topleft = self.pos
         if self.pos.x < 0:
@@ -42,5 +61,10 @@ class Paddle:
         if self.pos.x + paddle_size > sets.GAME_WINDOW_WIDTH:
             self.pos.x = sets.GAME_WINDOW_WIDTH - paddle_size
 
-    def render(self, surf):
+    def render(self, surf: pg.Surface) -> None:
+        """
+        Renders the paddle on the given surface.
+        Args:
+        surf (pg.Surface): The surface to render the paddle on.
+        """
         surf.blit(self.image, self.pos)
