3cad4d185a38a7611342bc8b255a254b5f0f2761
CaGListRo <paco.nyns@gmail.com>
2024-12-16 11:13:39 +0100

    Added type hints, return type hints and dochstrings to power_up.py and utilities.py. Added the Final statement to the settings.py.

--- power_up.py
+++ power_up.py
@@ -1,22 +1,36 @@
 import pygame as pg
 
-import settings as sets
+from typing import TypeVar, Final
 
-from random import randint
-
+Game = TypeVar("Game")
 
 
 class PowerUp:
-    def __init__(self, game, pos, img_number):
+    SIZE: Final[tuple[int]] = (50, 50)
+    
+    def __init__(self, game: Game, pos: tuple[int], img_number: int) -> None:
+        """
+        Initialize the PowerUp object.
+        Args:
+        game (Game): The game object.
+        pos (tuple[int]): The position of the PowerUp.
+        img_number (int): The image number of the PowerUp.
+        """
         self.game = game
         self.pos = list(pos)
-        self.image = pg.transform.scale(game.game_assets['pwr_ups'][img_number], (50, 50))
+        self.image = pg.transform.scale(game.game_assets['pwr_ups'][img_number], self.SIZE)
         self.rect = self.image.get_rect(midtop=self.pos)
         self.game.power_up_rects.append([self.rect, img_number])
 
-    def update(self):
+    def update(self) -> None:
+        """ Update the PowerUp position and the rect position. """
         self.pos[1] += 2
         self.rect.midtop = self.pos
 
-    def render(self, surf):
+    def render(self, surf: pg.Surface) -> None:
+        """
+        Render the PowerUp on the screen.
+        Args:
+        surf (pg.Surface): The screen surface.
+        """
         surf.blit(self.image, (self.pos))
--- utilities.py
+++ utilities.py
@@ -1,14 +1,29 @@
 import pygame as pg
 import os
 
-PATH = 'images/'
+BASE_PATH = 'images/'
 
-def load_image(path, img_name):
-    image = pg.image.load(PATH + path + '/' + img_name).convert_alpha()
+def load_image(path: str, img_name: str) -> pg.Surface:
+    """
+    Load image from file and return it as a pygame Surface object.
+    Args:
+    path (str): Path to the image file. BASE_PATH = 'images/'
+    img_name (str): Name of the image file.
+    Returns:
+    pg.Surface: Loaded image as a pygame Surface object.
+    """
+    image: pg.Surface = pg.image.load(BASE_PATH + path + '/' + img_name).convert_alpha()
     return image
 
-def load_images(path):
-    images = []
-    for img_name in sorted(os.listdir(PATH + path)):
+def load_images(path: str) -> list[pg.Surface]:
+    """
+    Load all images from a directory and return them as a list of pygame Surface objects.
+    Args:
+    path (str): Path to the directory containing the image files. BASE_PATH = 'images/
+    Returns:
+    list[pg.Surface]: List of loaded images as pygame Surface objects.
+    """
+    images: list[pg.Surface] = []
+    for img_name in sorted(os.listdir(BASE_PATH + path)):
         images.append(load_image(path, img_name))
     return images
